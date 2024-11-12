8201c85d5bf1b796998e0831e48dde98572f6127
bodyanept <red.suka.lantern@gmail.com>
2024-11-12 21:39:13 +0500

    4 lesson completed

---
+++ script.js
@@ -0,0 +1,49 @@
+//functions 
+
+// function sumNum(a,b){    // a,b параметры ф-ции
+//     console.log(a+b);
+// }
+// sumNum(4,5)              //4,5 -аргументы ф-ции
+
+
+// function sumNum2(a,b =1){
+//     console.log(a+b);
+
+// }
+
+// sumNum2(4,5)
+
+//task 1
+// let letter= 'aaaaaaaaaaaaaaaaaa'
+
+// function sayError(){
+//     alert(`'some error occured!' ${letter} ` )
+// }
+
+// sayError()
+
+
+
+//task2
+
+// function showError(x){
+//     alert(`error ${x} ocurred`)
+// }
+
+// showError('out of memory')
+// showError('to memory')
+
+
+//task3
+
+// function createHeaders(N){
+//     for(i=1;i<=N;i++)
+//     document.write('<h2> Header ' +i+'</h2>')
+// }
+
+// createHeaders(12)
+
+
+//task4
+
+
